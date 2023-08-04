9bd65eb475b63435d5e35685981f3095955ab6d7
aerobuff-anubhav <anubhav.gupta@colorado.edu>
2023-08-04 01:06:58 -0700

    trying css.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
